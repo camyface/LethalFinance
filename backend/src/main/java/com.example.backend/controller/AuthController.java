91f9897225722d4b8dab0636fa3b522a21a5cfba
Cameron Spencer <cameron.d.spencer3.mill@army.mil>
2026-04-20 13:44:25 -0500

    Secured credentials, updated gitignore, created controllers.

---
+++ backend/src/main/java/com.example.backend/controller/AuthController.java
@@ -0,0 +1,11 @@
+package com.example.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping()
+public class AuthController {
+
+}
